1525a9e603fbcc82a1ea93ff3c6a0ed27fd4089b
Mike Wittenauer <mikewittenauer@me.com>
2024-03-28 16:56:04 -0400

    Install mui joy

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import { useEffect, useState } from 'react';
+import { CssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Sheet from '@mui/joy/Sheet';
+import Typography from '@mui/joy/Typography';
+import Alert from '@mui/joy/Alert';
 
 function App() {
     const [notifications, setNotifications] = useState([]);
@@ -18,10 +23,30 @@ function App() {
         handleWebsocket();
     }, []);
     return (
-        <div className="App">
-            <h1>Twitter Notifications</h1>
-            { notifications.map(n => <div>{ JSON.stringify(n) }</div>) }
-        </div>
+        <CssVarsProvider>
+            <CssBaseline />
+            <Sheet sx={{
+                width: '80%',
+                mx: 'auto', // margin left & right
+                my: 4, // margin top & bottom
+                py: 3, // padding top & bottom
+                px: 2, // padding left & right
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+                borderRadius: 'sm',
+                boxShadow: 'md',
+            }}
+            variant='outlined'>
+                <Typography level="h1" component="h1">Twitter Notifications</Typography>
+                { notifications.map(n => 
+                    <>
+                        <Alert variant='outlined'><strong>@opensourceDet</strong> liked <strong>@{n.username}'s</strong> Tweet.</Alert>
+                    </>    
+                )
+                }
+            </Sheet>
+        </CssVarsProvider>
     );
 }
 
