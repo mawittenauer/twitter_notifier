9b156ba051ba9b6dc3e53b7ff6224347a436153b
Mike Wittenauer <mikewittenauer@me.com>
2024-03-28 10:17:40 -0400

    Add some basic formatting to the console output

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ app.use(bodyParser.json());
 const PORT = 3000;
 
 app.post('/hook', (req, res) => {
-    console.log(req);
     console.log(req.body);
+    const { type } = req.body;
+
+    if (type === 'like') {
+        const { username } = req.body;
+        console.log(`%c You liked a tweet posted by @${username}`);
+    }
+
+    if (type === 'follower') {
+        const { username } = req.body;
+        console.log(`You got a new follower! @${username}`);
+    }
+
     res.status(200).end();
 });
 
